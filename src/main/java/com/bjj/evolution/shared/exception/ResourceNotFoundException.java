8d92b66f3f6b9d0617b6bc2715db6292736ffef3
Afonso Pinheiro <afonsoprad10@gmail.com>
2026-05-15 17:47:13 -0400

    Feat: add specifc exceptions

---
+++ src/main/java/com/bjj/evolution/shared/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package com.bjj.evolution.shared.exception;
+
+import java.util.UUID;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String resource, UUID id) {
+        super(resource + " not found with id: " + id);
+    }
+
+    public ResourceNotFoundException(String resource, Long id) {
+        super(resource + " not found with id: " + id);
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
